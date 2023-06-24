e993b00d6e3bb17b6f2aaee11e608a556c9ce4a4
Marley Eduardo <116856059+Marleyedrg@users.noreply.github.com>
2023-06-24 01:04:58 -0300

    barrers

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let squares = document.querySelectorAll(".squares");
 let barrer = document.querySelectorAll(".space-btw-sqr");
 
+squareProperty = {
+  top:true,
+  right:true,
+  bottom:true,
+  ledt:true,
+}
+
 const player1 = {
   playerName: 'player1',
   PlayerPosition: [-4,4],
@@ -33,7 +40,7 @@ const player2 = {
 let currentPlayer;
 let turn = 1;
 
-if(turn === 1){
+if(turn === 1){//initial state
   currentPlayer = player1;
 }else if(turn === 2){
   currentPlayer = player2;
@@ -50,26 +57,101 @@ const ChangeTurn = function(currentTurn){
   }
 }
 
+const hasABarrierWithThisPosition = function(currentPos,clickedPosition , XOrYMove,directionOperation){
+  let barrerId = [];
+  barrerId = [...currentPos];
+  let v;
+  
+  if(XOrYMove === 'x'){
+
+    if(directionOperation === 'negative'){
+
+      barrerId = (`${barrerId[0]+0.5}/${barrerId[1]}`)
+
+    }else if (directionOperation === 'positive'){
+
+      barrerId = (`${barrerId[0]+0.5}/${barrerId[1]}`)
+    }
+
+  }else if(XOrYMove === 'y'){
+
+    if(directionOperation === 'negative'){
+
+      barrerId = (`${barrerId[0]}/${barrerId[1]+0.5}`)
+
+    }else if (directionOperation === 'positive'){
+
+      barrerId = (`${barrerId[0]}/${barrerId[1]+0.5}`)
+
+    }
+  }
+
+  let barrerPosition = barrerId.split('/');
+  barrerPosition = [Number(barrerPosition[0]),Number(barrerPosition[1])]
+
+    if(document.getElementById(barrerId)){
+      return true;
+    }else{
+      return false;
+    }
+}
+
 const movementValidation = function(currentPos,clickedPosition){
   /**
    * this method "Math.abs" ignores the sign of the number, so we don't care about the direction of the movement, but  whether its absolute value was just one movement.Thanks chatgpt :)
    * */
   let deltaX = Math.abs(clickedPosition[0] - currentPos[0]);
   let deltaY = Math.abs(clickedPosition[1] - currentPos[1]);
-  if((deltaX === 1 && deltaY === 0)||(deltaX === 0 && deltaY === 1)){
-    return true;
+  
+  if(deltaX === 1 && deltaY === 0){// x move
+    if(currentPos[0] < 0){
+      if(hasABarrierWithThisPosition(currentPos,clickedPosition, 'x', 'negative')){
+        return true;
+      }else{
+        return false;
+      }
+
+    }else if(currentPos[0] >= 0){
+
+      if(hasABarrierWithThisPosition(currentPos,clickedPosition, 'x','positive')){
+        return true;
+      }else{
+        return false;
+      }
+
+    }
+  }else if(deltaX === 0 && deltaY === 1){// y move
+
+    if(currentPos[1] < 0){
+      if(hasABarrierWithThisPosition(currentPos,clickedPosition, 'y', 'negative')){
+        return true;
+      }else{
+        return false;
+      }
+
+    }else if(currentPos[1] >= 0){
+
+      if(hasABarrierWithThisPosition(currentPos,clickedPosition, 'y','positive')){
+        return true;
+      }else{
+        return false;
+      }
+    }
   }else{
     return false;
   }
 
 }
+
 barrer.forEach(barrer =>{
   barrer.addEventListener("click", () =>{
     if(turn === 1 && barrer.style.backgroundColor === ""){
       barrer.style.backgroundColor = "black"
+      barrer.id = '';
       ChangeTurn(turn);
     }else if(turn === 2 && barrer.style.backgroundColor === ""){
       barrer.style.backgroundColor = "red"
+      barrer.id = '';
       ChangeTurn(turn);
     }
   });
