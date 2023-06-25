ea79c7a5a50c9b0cd171e2ef8af4155f3f0d7b12
Marley Eduardo <116856059+Marleyedrg@users.noreply.github.com>
2023-06-25 18:02:16 -0300

    beta 1.1

--- script.js
+++ script.js
@@ -105,7 +105,8 @@ const movementValidation = function(currentPos,clickedPosition){
   
   if(deltaX === 1 && deltaY === 0){// x move
 
-    if(clickedPosition[0] > currentPos[0] && currentPos[0] !== 0){
+    if(clickedPosition[0] > currentPos[0] && currentPos[0] !== 0 ||
+       clickedPosition[0] > currentPos[0] && currentPos[0] === 0){
       let possibleBarrer = (clickedPosition[0] - 0.5);
       possibleBarrer = (`${possibleBarrer}/${clickedPosition[1]}`);
       console.log(`${possibleBarrer} 1`)
@@ -118,7 +119,8 @@ const movementValidation = function(currentPos,clickedPosition){
         false;
       }
 
-    }else if(clickedPosition[0] < currentPos[0] && currentPos[0] !== 0){
+    }else if(clickedPosition[0] < currentPos[0] && currentPos[0] !== 0 || 
+      clickedPosition[0] < currentPos[0] && currentPos[0] === 0){
       let possibleBarrer = (clickedPosition[0] + 0.5);
       possibleBarrer = (`${possibleBarrer}/${clickedPosition[1]}`);
       console.log(possibleBarrer)
@@ -133,7 +135,8 @@ const movementValidation = function(currentPos,clickedPosition){
     }
   }else if(deltaX === 0 && deltaY === 1){// y move
 
-      if(clickedPosition[1] > currentPos[1] || currentPos[1] !== 0){
+      if(clickedPosition[1] > currentPos[1] && currentPos[1] !== 0 ||
+         clickedPosition[1] > currentPos[1] && currentPos[1] === 0){
         let possibleBarrer = (clickedPosition[1] - 0.5);
         possibleBarrer = (`${clickedPosition[0]}/${possibleBarrer}`);
         console.log(possibleBarrer)
@@ -145,7 +148,8 @@ const movementValidation = function(currentPos,clickedPosition){
           false;
         }
 
-      }else if(clickedPosition[1] < currentPos[1] && currentPos[1] === 0){
+      }else if(clickedPosition[1] < currentPos[1] && currentPos[1] !== 0 ||
+        clickedPosition[1] < currentPos[1] && currentPos[1] === 0){
         let possibleBarrer = (clickedPosition[1] + 0.5);
         possibleBarrer = (`${clickedPosition[0]}/${possibleBarrer}`);
         console.log(possibleBarrer)
